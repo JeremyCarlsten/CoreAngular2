2e9911d154876599d614a85b0fb08ef63d3e6346
Jeremy Carlsten <jcc.993@gmail.com>
2016-09-26 17:06:54 +0000

    Getting the sample project up and running

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import {Http} from '@angular/http';
+import 'rxjs/Rx'; // For methods for Observables
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  server = 'https://angular-jeremycarlsten.c9users.io:8081';
+  people = [];
+
+  constructor(private https: Http) {
+  }
+
+  checkSearch(term) {
+    if (term.length < 2) {
+      this.people = [];
+    } else {
+      this.https.get(this.server + '/people/' + term)
+        .map((res) => res.json())
+        .subscribe((response) => {
+          this.people = response.people;
+        });
+    }
+  }
 }
